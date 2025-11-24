30a0affb3d7409c983e3648a683b189f2d996c3c
Aditi Shukla <aditishukla1676@gmail.com>
2025-11-24 15:11:37 +0530

    first commit

---
+++ apps/pooler/src/connectionredis/connectredis.ts
@@ -0,0 +1,1 @@
+import { createClient, type Redis } from "redis";
---
+++ apps/pooler/src/index.ts
@@ -0,0 +1,12 @@
+﻿import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_req, res) => {
+  res.send("pooler: alive — ts-node & nodemon working ✅");
+});
+
+app.listen(PORT, () => {
+  console.log(`pooler listening on http://localhost:${PORT}`);
+});
